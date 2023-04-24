c2db12f27f1c3f42e0184cd9bddef6eb66aed643
Urszula Majcher <ulunka.m@gmail.com>
2023-04-24 21:16:40 +0200

    task list without interactions yet

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Form from "./Form";
+import Tasks from "./Tasks";
+import Buttons from "./Buttons";
+import Section from "./Section";
+import Header from "./Header";
+import Main from "./Main";
+
+const tasks = [
+  { id: 1, content: "przejść na Reacta", done: false },
+  { id: 2, content: "zjeść obiad", done: true },
+];
+
+const hideDoneTasks = false;
+
+function App() {
+  return (
+    <Main>
+     <Header title="Lista zadań" />
+
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+      />
+
+    </Main>
+  );
+}
+
+export default App;
